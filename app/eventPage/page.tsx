b4d09bab4f0e2d92ef578f5e81dc7a6abea8fda5
Arthur Ribeiro <arthursnr2021@gmail.com>
2025-02-04 01:03:49 -0300

    event page for each event, a bigger event card for better view

---
+++ app/eventPage/page.tsx
@@ -0,0 +1,11 @@
+export default function EventPage() {
+
+    return (
+        <div>
+            <img src="" alt="Event Image" />
+            <h1>event title</h1>
+            <p>event description</p>
+            <a href="https://www.google.com"></a>
+        </div>
+    )
+}
