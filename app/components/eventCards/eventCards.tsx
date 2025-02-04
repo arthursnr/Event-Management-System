0332877cb646d9f4638d594f07bd7c8f31470385
Arthur Ribeiro <arthursnr2021@gmail.com>
2025-02-04 01:02:03 -0300

    event individual card

---
+++ app/components/eventCards/eventCards.tsx
@@ -0,0 +1,21 @@
+"use client"
+
+import { useRouter } from "next/navigation"
+
+export function EventCards() {
+    const router = useRouter();
+
+    const eventPage = () => {
+        router.push("/eventPage");
+    }
+
+    return (
+        <button
+            onClick={eventPage}
+            className="flex flex-col bg-slate-600 px-6 pt-8 pb-4 rounded-lg">
+            <img src="" alt="Event Image" />
+            <h1>event title</h1>
+            <p>event description</p>
+        </button>
+    )
+}
