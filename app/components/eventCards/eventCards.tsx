1bb2b5d867919aca82a3520cb33f182d7704e93d
Arthur Ribeiro <arthursnr2021@gmail.com>
2025-02-05 00:08:48 -0300

    no major change

--- app/components/eventCards/eventCards.tsx
+++ app/components/eventCards/eventCards.tsx
@@ -13,7 +13,7 @@ export function EventCards() {
         <button
             onClick={eventPage}
             className="flex flex-col bg-slate-600 px-6 pt-8 pb-4 rounded-lg">
-            <img src="" alt="Event Image" />
+            <img src="/assets/img1.png" alt="Event Image" />
             <h1>event title</h1>
             <p>event description</p>
         </button>
