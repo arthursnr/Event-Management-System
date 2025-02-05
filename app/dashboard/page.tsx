8a4400fe5611671979e46bbf837ad70ba35147a5
Arthur Ribeiro <arthursnr2021@gmail.com>
2025-02-05 00:09:47 -0300

    header and footer added

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,20 +1,27 @@
-import { NewEventButton } from "../components/buttons/newEventButton/newEventButton";
+import { MenuButton } from "../components/buttons/menuButton/menuButton";
 import { EventCards } from "../components/eventCards/eventCards";
 
 export default function DashBoard() {
     return (
-        <>
-            <div className="">
-                <div>
-                    <header className="flex gap-8">
-                        <h1>Events Management System</h1>
-                        <NewEventButton />
-                    </header>
-                    <div className="border-2 shadow-2xl w-screen h-screen">
-                        <EventCards />
-                    </div>
-                </div>
+        <div className="h-screen w-screen bg-slate-400">
+            <div className="w-screen">
+                <header className="flex text-xl leading-none font-bold bg-white shadow-md pt-14 pb-6 px-7 gap-4">
+                    <h1>Events Management System</h1>
+                    <MenuButton />
+                </header>
             </div>
-        </>
+            <div className="bg-slate-400 shadow-2xl w-screen h-screen">
+                <EventCards />
+            </div>
+            <div className="w-screen flex items-center justify-center">
+                <footer className="bg-white py-3 px-4 flex justify-center items-center fixed bottom-0 w-full">
+                    <ul className="flex items-center justify-between w-full flex-wrap gap-1 font-normal text-sm leading-none">
+                        <p className="text-gray-600 text-sm font-bold">Event Management System</p>
+                        <p className="text-[#333333] text-[10px] font-normal">© 2025 - All rights reserved.</p>
+                    </ul>
+                </footer>
+            </div>
+
+        </div>
     )
 }
