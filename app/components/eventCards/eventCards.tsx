a64d3b2e3b9df1556625365c59dc686082666321
Arthur Ribeiro <arthursnr2021@gmail.com>
2025-02-08 14:40:58 -0300

    images on the event cards

--- app/components/eventCards/eventCards.tsx
+++ app/components/eventCards/eventCards.tsx
@@ -10,46 +10,46 @@ export function EventCards() {
     }
 
     return (
-            <div className="flex  flex-wrap gap-2 items-center justify-center">
-                <div className="bg-[#E0E0E0] text-[#333333] rounded-lg">
-                    <button
-                        onClick={eventPage}
-                        className="flex flex-col h-60 px-6 pt-8 pb-4  items-center justify-center">
-                        <img src="/assets/img1.png" alt="Event Image" />
-                        <h1>event title</h1>
-                        <p>event description</p>
-                    </button>
-                </div>
-
-                <div className="bg-[#E0E0E0] text-[#333333] rounded-lg">
-                    <button
-                        onClick={eventPage}
-                        className="flex flex-col h-60 px-6 pt-8 pb-4 items-center justify-center">
-                        <img src="/assets/img1.png" alt="Event Image" />
-                        <h1>event title</h1>
-                        <p>event description</p>
-                    </button>
-                </div>
-
-                <div className="bg-[#E0E0E0] text-[#333333] rounded-lg">
-                    <button
-                        onClick={eventPage}
-                        className="flex flex-col h-60 px-6 pt-8 pb-4 items-center justify-center">
-                        <img src="/assets/img1.png" alt="Event Image" />
-                        <h1>event title</h1>
-                        <p>event description</p>
-                    </button>
-                </div>
-
-                <div className="bg-[#E0E0E0] text-[#333333] rounded-lg">
-                    <button
-                        onClick={eventPage}
-                        className="flex flex-col h-60 px-6 pt-8 pb-4 items-center justify-center">
-                        <img src="/assets/img1.png" alt="Event Image" />
-                        <h1>event title</h1>
-                        <p>event description</p>
-                    </button>
-                </div>
+        <div className="flex  flex-wrap gap-2 items-center justify-center">
+            <div className="bg-[#E0E0E0] text-[#333333] rounded-lg">
+                <button
+                    onClick={eventPage}
+                    className="flex flex-col h-60 px-6 pt-8 pb-4  items-center justify-center">
+                    <img src="/assets/img/nicolas.jpg" alt="Event Image" className="size-20"/>
+                    <h1>event title</h1>
+                    <p>event description</p>
+                </button>
             </div>
+
+            <div className="bg-[#E0E0E0] text-[#333333] rounded-lg">
+                <button
+                    onClick={eventPage}
+                    className="flex flex-col h-60 px-6 pt-8 pb-4 items-center justify-center">
+                    <img src="/assets/img/nicolas.jpg" alt="Event Image" className="size-20"/>
+                    <h1>event title</h1>
+                    <p>event description</p>
+                </button>
+            </div>
+
+            <div className="bg-[#E0E0E0] text-[#333333] rounded-lg">
+                <button
+                    onClick={eventPage}
+                    className="flex flex-col h-60 px-6 pt-8 pb-4 items-center justify-center">
+                    <img src="/assets/img/nicolas.jpg" alt="Event Image" className="size-20"/>
+                    <h1>event title</h1>
+                    <p>event description</p>
+                </button>
+            </div>
+
+            <div className="bg-[#E0E0E0] text-[#333333] rounded-lg">
+                <button
+                    onClick={eventPage}
+                    className="flex flex-col h-60 px-6 pt-8 pb-4 items-center justify-center">
+                    <img src="/assets/img/nicolas.jpg" alt="Event Image" className="size-20" />
+                    <h1>event title</h1>
+                    <p>event description</p>
+                </button>
+            </div>
+        </div>
     )
 }
